bfa9ca11cc479bb1cd1f1571da9d1a4df802a5f4
deanefraim1 <deanefraim@campus.technion.ac.il>
2023-04-09 10:20:21 +0300

    .

--- magic_get_wand_syscall.c
+++ magic_get_wand_syscall.c
@@ -2,20 +2,9 @@
 #include <linux/sched.h>
 #include <linux/list.h>
 
+#define SECRET_MAXSIZE 128
+
 int magic_get_wand(int power, char secret[SECRET_MAXSIZE])
 {
-    struct task_struct *task;
-    struct list_head *list;
-    int i = 0;
-
-    list_for_each(list, &current->children) {
-        task = list_entry(list, struct task_struct, sibling);
-        if (task->pid == power) {
-            for (i = 0; i < SECRET_MAXSIZE; i++) {
-                secret[i] = task->secret[i];
-            }
-            return 0;
-        }
-    }
-    return -1;
+    
 }
